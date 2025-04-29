f127e4fa4022534c4de011de0facbc14626dcafc
harsha <mohan_mothukayala@epam.com>
2025-04-29 15:13:06 +0530

    renamed the main class

---
+++ src/main/java/org/example/GitHubActionTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubActionTest {
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubActionTest.class, args);
+    }
+}
